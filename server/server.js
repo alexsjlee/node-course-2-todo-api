c73c86deb6441ae30fd2284f2559727e5f4db26d
alexsjlee <sjlee89@gmail.com>
2017-07-10 10:59:09 -0700

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -36,4 +36,4 @@ app.listen(port, () => {
 
 module.exports = {
     app
-}
+};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,21 @@ const request = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+const todos = [
+    {
+        text: 'First test todo'
+    },
+    {
+        text: 'Second test todo'
+    }
+];
+
 // Testing lifecycle method 'beforeEach' lets us run some code before each test
 beforeEach((done) => {
     // Just emptying the database before each test call because we are checking the todo.length to be 1
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -34,7 +45,7 @@ describe('POST /todos', () => {
                 }
                 
                 // Calls all of the todos
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     // Expecting a length of 1
                     expect(todos.length).toBe(1);
                     // Expecting that one todo[0].text is there and that its type is text
@@ -59,11 +70,23 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     done(e);
                 });
             });
+    });
+});
+
+describe('GET /todos', () => {
+    it('shoudl get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
