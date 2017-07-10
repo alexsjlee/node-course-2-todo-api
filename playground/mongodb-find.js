c8255334dbde9c259d493c17717ffa7aafeab032
alexsjlee <sjlee89@gmail.com>
2017-07-09 19:28:32 -0700

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server')
+    }
+    console.log('Connected to MongoDB server')
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5962e1751a4dde28a7848c41')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Alex'}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+    db.close();
+});
